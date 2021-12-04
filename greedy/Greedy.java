e37018ccb44d85d31311534bd34477d5b97c027e
Igor Sivolobov <swebdesigne@gmail.com>
2021-12-04 22:00:40 +0300

    Жадный алгоритм

---
+++ greedy/Greedy.java
@@ -0,0 +1,24 @@
+package greedy;
+
+import java.util.Arrays;
+
+public class Greedy {
+    public int[] execute(int[] coins, int money, int price) {
+        int size = 0;
+        int[] rsl = new int[100];
+        int remainder = money - price;
+        for (int i = 0; i < coins.length; i++) {
+            while (remainder >= coins[i]) {
+                remainder -= coins[i];
+                rsl[size] = coins[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+
+    public static void main(String[] args) {
+        Arrays.stream(new Greedy().execute(new int[] {10, 5, 2, 1}, 121, 50))
+                .forEach(System.out::println);
+    }
+}
---
+++ greedy/GreedyTest.java
@@ -0,0 +1,15 @@
+package greedy;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class GreedyTest {
+    @Test
+    public void execute() {
+            int[] expected = {10, 5};
+            assertThat(new Greedy().execute(new int[] {10, 5, 2, 1},
+                    50, 35), is(expected));
+    }
+}
